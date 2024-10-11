3b7be3c97648b786627447b32d97a9ae03a8659f
Muhammad Shan <150339563+Mshandev@users.noreply.github.com>
2024-10-11 12:07:05 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,30 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "utfs.io" }],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all API routes
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate",
+          },
+        ],
+      },
+      {
+        // If you want to prevent caching for all pages
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
